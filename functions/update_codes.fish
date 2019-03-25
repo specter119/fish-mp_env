560064fc8189279d7945146f0f9672c60a851fbc
specter119 <specter119@gmail.com>
2019-03-25 15:30:24 +0800

    fix update_codes

--- functions/update_codes.fish
+++ functions/update_codes.fish
@@ -2,15 +2,6 @@
 
 function update_codes -d 'Update packages from local git repos'
   if set -q $argv[1]
-    set code_path $MP_CODES_ROOT/$i
-    if [ -d $code_path ]
-      echo -e "\nUpdating $i in $code_path:"
-      cd $code_path
-      git pull
-      pip install -e .
-      cd $OLDPWD
-    end
-  else
     conda upgrade --all -y
     conda clean --index-cache --packages --tarballs -y
     set modules_backup $LOADEDMODULES
@@ -19,5 +10,14 @@ function update_codes -d 'Update packages from local git repos'
       update_codes $i
     end
     module load (string split ':' -- $modules_backup)
+  else
+    set code_path $MP_CODES_ROOT/$i
+    if [ -d $code_path ]
+      echo -e "\nUpdating $i in $code_path:"
+      cd $code_path
+      git pull
+      pip install -e .
+      cd $OLDPWD
+    end
   end
 end
