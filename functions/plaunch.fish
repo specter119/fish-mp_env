4a9f5d5a27ba787018d0d8dcc54b8efddba1a270
specter119 <specter119@gmail.com>
2019-03-29 17:12:57 +0800

    fix {p,s}launch

--- functions/plaunch.fish
+++ functions/plaunch.fish
@@ -4,7 +4,7 @@ function plaunch -d 'Launch calculations background continuously'
   set -q MP_LAUNCH_ROOT[1]; or set MP_LAUNCH_ROOT $HOME/blocks
   cd $MP_LAUNCH_ROOT
   set -q $argv[1]; and set queue_len $argv[1]; or set queue_len 4
-  if [ ( screen -ls | grep $MP_SCREEN_NAME | wc ) -eq 0 ]
+  if [ ( screen -ls | grep $MP_SCREEN_NAME | wc -l ) -eq 0 ]
     screen -dmS $MP_SCREEN_NAME qlaunch -r rapidfire --nlaunches infinite -m $queue_len --sleep 30 -b 10000
   end
   cd $OLDPWD
--- functions/slaunch.fish
+++ functions/slaunch.fish
@@ -3,7 +3,11 @@
 function slaunch -d 'Launch one or more calculations'
   set -q MP_LAUNCH_ROOT[1]; or set MP_LAUNCH_ROOT $HOME/blocks
   cd $MP_LAUNCH_ROOT
-  set -q $argv[1]; and set n_launcher $argv[1]; or set n_launcher 1
+  # set -q $argv[1]; and set n_launcher $argv[1]; or set n_launcher 1
+  if set -q $argv[1]
+    set n_launcher $argv[1]
+  else
+    set n_launcher 1
   echo "n_launcher is $n_launcher"
   qlaunch -r rapidfire --nlaunches $n_launcher -b 10000
   cd $OLDPWD
