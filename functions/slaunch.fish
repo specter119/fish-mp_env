9dd8a3fcc893bc249d837b8c4d529b0f1574fdd9
specter119 <specter119@gmail.com>
2019-03-29 17:31:40 +0800

    remove debug message for slaunch

--- functions/slaunch.fish
+++ functions/slaunch.fish
@@ -4,7 +4,6 @@ function slaunch -d 'Launch one or more calculations'
   set -q MP_LAUNCH_ROOT[1]; or set MP_LAUNCH_ROOT $HOME/blocks
   cd $MP_LAUNCH_ROOT
   set -q $argv[1]; and set n_launcher 1; or set n_launcher $argv[1]
-  echo "n_launcher is $n_launcher"
   qlaunch -r rapidfire --nlaunches $n_launcher -b 10000
   cd $dirprev[-1]
 end
