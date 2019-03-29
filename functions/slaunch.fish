c3a5dc98a0bd6eb6dd05f948470356b71a004134
specter119 <specter119@gmail.com>
2019-03-29 16:13:20 +0800

    fix slaunch

--- functions/slaunch.fish
+++ functions/slaunch.fish
@@ -3,7 +3,7 @@
 function slaunch -d 'Launch one or more calculations'
   set -q MP_LAUNCH_ROOT[1]; or set MP_LAUNCH_ROOT $HOME/blocks
   cd $MP_LAUNCH_ROOT
-  set -q $argv[1]; set n_launcher $argv[1] or set n_launcher 4
+  set -q $argv[1]; set n_launcher 4 or set n_launcher $argv[1]
   qlaunch -r rapidfire --nlaunches $n_launcher -b 10000
   cd $OLDPWD
 end
